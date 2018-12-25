43a754810cfbafe3f7ae822b285feac4ff0d0251
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2018-12-25 13:54:16 -0600

    Completed work through numbers and strings

---
+++ python_level_one/numbers/numbers.py
@@ -0,0 +1,27 @@
+print(4 * 4)
+print(4 ** 0.5)
+print(2 + 10 * 10 + 3)
+print(8 ** 2)
+
+print("=================")
+
+a = 5
+print(a)
+# comment
+# variable names cannot start with a number, i.e. 2dogs is an invalid variable name
+# 2dogs = 2
+# File "numbers.py", line 12
+#    2dogs = 2
+#        ^
+# SyntaxError: invalid syntax
+
+# Common python variable naming syntax is snake_case rather than camelCase
+two_dogs = 2
+print(two_dogs)
+
+print("=================")
+
+my_income = 20000
+my_tax_rate = 0.1
+my_taxes = my_income * my_tax_rate
+print(my_taxes)
